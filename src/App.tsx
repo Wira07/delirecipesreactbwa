b3dbc3005fc1d170b552739bd476874f1364d550
Wira07 <wiralodrasaputra07@gmail.com>
2024-09-20 23:45:50 +0700

    refactor: Update App.tsx, Browse.tsx, and RecipeCardResult.tsx components
    - Update App.tsx to add SearchDetails component and remove commented out routes
    - Update Browse.tsx to add search functionality and handle search query
    - Update RecipeCardResult.tsx to add RecipeCardResult component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Browse from "./pages/Browse";
+import SearchDetails from "./pages/Searchdetails";
+
 // import RecipeDetails from './pages/RecipeDetails';
 // import CategoryDetails from './pages/CategoryDetails';
 // import SearchDetails from './pages/SearchDetails';
@@ -9,8 +11,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Browse />} />
-        {/* <Route path="/search" element={<SearchDetails />} />
-        <Route path="/recipe/:slug" element={<RecipeDetails />} />
+        <Route path="/search" element={<SearchDetails />} />
+        {/* <Route path="/recipe/:slug" element={<RecipeDetails />} />
         <Route path="/category/:slug" element={<CategoryDetails />} /> */}
       </Routes>
     </Router>
