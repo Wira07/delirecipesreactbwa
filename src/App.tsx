1652851d59ccda9a0b667f696c4171ce647abdc3
Wira07 <wiralodrasaputra07@gmail.com>
2024-10-20 06:59:51 +0700

    refactor: Update App.tsx, Browse.tsx, and RecipeCardResult.tsx components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Browse from "./pages/Browse";
-import SearchDetails from "./pages/Searchdetails";
+import CategoryDetails from "./pages/CategoryDetails";
+import SearchDetails from "./pages/SearchDetails";
 
 // import RecipeDetails from './pages/RecipeDetails';
 // import CategoryDetails from './pages/CategoryDetails';
@@ -12,8 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Browse />} />
         <Route path="/search" element={<SearchDetails />} />
-        {/* <Route path="/recipe/:slug" element={<RecipeDetails />} />
-        <Route path="/category/:slug" element={<CategoryDetails />} /> */}
+        {/* <Route path="/recipe/:slug" element={<RecipeDetails />} /> */}
+        <Route path="/category/:slug" element={<CategoryDetails />} />
       </Routes>
     </Router>
   );
