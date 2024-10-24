f10505e2683c6e69c630c21ea5ab77d2d72acf93
Wira07 <wiralodrasaputra07@gmail.com>
2024-10-24 20:14:09 +0700

    Refactor RecipeDetails page to fetch recipe data and display dynamic content

--- src/pages/RecipeDetails.tsx
+++ src/pages/RecipeDetails.tsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import IngredientCard from "../components/IngredientCard";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import { Recipe } from "../types/type";
 
 export default function RecipeDetails() {
   const [activeTab, setActiveTab] = useState("ingredients");
@@ -9,21 +12,52 @@ export default function RecipeDetails() {
     setActiveTab(tab);
   };
 
+  const { slug } = useParams<{ slug: string }>();
+  const [recipe, setRecipe] = useState<Recipe | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios
+      .get(`http://delirecipesbwa.test/api/recipe/${slug}`)
+      .then((response) => {
+        setRecipe(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, [slug]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error loading recipe: {error}</p>;
+  }
+
+  if (!recipe) {
+    return <p>Recipe not found</p>;
+  }
+
+  const baseURL = "http://delirecipesbwa.test/storage";
+
   return (
     <>
       <nav className="absolute top-0 flex w-full max-w-[640px] items-center justify-between px-5 mt-[30px] z-20">
-        <a href="index.html">
+        <Link to={"/"}>
           <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white/20">
             <img src="/assets/images/icons/arrow-left.svg" className="w-5 h-5 object-contain" alt="icon" />
           </div>
-        </a>
+        </Link>
         <button className="appearance-none">
           <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white/20">
             <img src="/assets/images/icons/heart.svg" className="w-5 h-5 object-contain" alt="icon" />
           </div>
         </button>
       </nav>
-
       <header id="Gallery" className="relative w-full h-[520px] flex shrink-0 rounded-b-[40px] bg-black overflow-hidden">
         <div className="swiper">
           <div className="swiper-wrapper">
@@ -31,21 +65,17 @@ export default function RecipeDetails() {
               <SwiperSlide>
                 <div className="relative w-full h-full flex shrink-0">
                   <div className="gradient-filter absolute w-full h-full bg-[linear-gradient(180deg,rgba(0,0,0,0)40.47%,#000000_81.6%)] z-10"></div>
-                  <img src="/assets/images/thumbnails/thumbnail-2.png" className="w-full h-full object-cover" alt="thumbnail" />
+                  <img src={`${baseURL}/${recipe.thumbnail}`} className="w-full h-full object-cover" alt="thumbnail" />
                 </div>
               </SwiperSlide>
-              <SwiperSlide>
-                <div className="relative w-full h-full flex shrink-0">
-                  <div className="gradient-filter absolute w-full h-full bg-[linear-gradient(180deg,rgba(0,0,0,0)40.47%,#000000_81.6%)] z-10"></div>
-                  <img src="/assets/images/thumbnails/thumbnail-1.png" className="w-full h-full object-cover" alt="thumbnail" />
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="relative w-full h-full flex shrink-0">
-                  <div className="gradient-filter absolute w-full h-full bg-[linear-gradient(180deg,rgba(0,0,0,0)40.47%,#000000_81.6%)] z-10"></div>
-                  <img src="/assets/images/thumbnails/thumbnail-3.png" className="w-full h-full object-cover" alt="thumbnail" />
-                </div>
-              </SwiperSlide>
+              {recipe.photos.map((photo) => (
+                <SwiperSlide>
+                  <div className="relative w-full h-full flex shrink-0">
+                    <div className="gradient-filter absolute w-full h-full bg-[linear-gradient(180deg,rgba(0,0,0,0)40.47%,#000000_81.6%)] z-10"></div>
+                    <img src={`${baseURL}/${photo.photo}`} className="w-full h-full object-cover" alt="thumbnail" />
+                  </div>
+                </SwiperSlide>
+              ))}
             </Swiper>
           </div>
         </div>
@@ -54,8 +84,8 @@ export default function RecipeDetails() {
           <div className="swiper-pagination !-top-5 *:!bg-white"></div>
           <div className="flex justify-between p-5 pb-[23px] gap-3">
             <div className="flex flex-col gap-[6px]">
-              <p className="font-semibold text-[#FF4C1C]">Top Bakery</p>
-              <h1 className="font-bold text-[34px] leading-[46px] text-white">Burger Tebal Makin Hot</h1>
+              <p className="font-semibold text-[#FF4C1C]">Top {recipe.category.name}</p>
+              <h1 className="font-bold text-[34px] leading-[46px] text-white">{recipe.name}</h1>
             </div>
             <div className="flex shrink-0 items-center w-fit h-fit rounded-full py-1 px-2 bg-white/20 backdrop-blur">
               <img src="/assets/images/icons/Star 1.svg" className="w-4 h-4" alt="star" />
@@ -68,15 +98,15 @@ export default function RecipeDetails() {
       <section id="Description" className="flex flex-col gap-4 px-5 mt-[30px]">
         <div className="flex flex-col gap-2">
           <h2 className="font-bold">About</h2>
-          <p className="leading-8">Burger tebal asal kota inkopad ini sangat membuat lapar dan sehat untuk tubuh kita terutama ketika sedang bulking pada masa otot terbaru setelah olahraga.</p>
+          <p className="leading-8">{recipe.about}</p>
         </div>
         <div className="flex items-center justify-between gap-3">
           <div className="flex items-center gap-3">
             <div className="flex shrink-0 w-[50px] h-[50px] rounded-full overflow-hidden">
-              <img src="/assets/images/photos/photo-1.png" className="w-full h-full object-cover" alt="avatar" />
+              <img src={`${baseURL}/${recipe.author.photo}`} className="w-full h-full object-cover" alt="avatar" />
             </div>
             <div className="flex flex-col gap-[2px]">
-              <p className="font-semibold">Shayna</p>
+              <p className="font-semibold">{recipe.author.name}</p>
               <p className="text-sm leading-[21px] text-[#848486]">Author</p>
             </div>
           </div>
